5534500b2d01ce18ea4e4f62173e0e928cd45c9a
Anique Kumar Das <aniquedas512@gmail.com>
2026-07-17 22:33:33 +0600

    Real Madrid

---
+++ Madrid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "==============================" << endl;
+    cout << "      REAL MADRID C.F." << endl;
+    cout << "==============================" << endl;
+
+    cout << "Club Name : Real Madrid Club de Futbol" << endl;
+    cout << "Country   : Spain" << endl;
+    cout << "City      : Madrid" << endl;
+    cout << "Founded   : 1902" << endl;
+    cout << "Stadium   : Santiago Bernabeu" << endl;
+    cout << "Coach     : Xabi Alonso" << endl;
+
+    cout << "\nHala Madrid!" << endl;
+
+    return 0;
+}
