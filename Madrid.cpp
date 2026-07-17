069aa8163123945893a154e1f2cf09776bec351d
Anique Kumar Das <aniquedas512@gmail.com>
2026-07-17 22:56:06 +0600

    Updated by Aniq

--- Madrid.cpp
+++ Madrid.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main() {
 
-    cout << "==============================" << endl;
-    cout << "      REAL MADRID C.F." << endl;
-    cout << "==============================" << endl;
 
     cout << "Club Name : Real Madrid Club de Futbol" << endl;
     cout << "Country   : Spain" << endl;
     cout << "City      : Madrid" << endl;
     cout << "Founded   : 1902" << endl;
     cout << "Stadium   : Santiago Bernabeu" << endl;
-    cout << "Coach     : Xabi Alonso" << endl;
+    cout << "Coach     : José Mourinho" << endl;
+    cout << "Captain   : Federico Valverde" << endl;
 
-    cout << "\nHala Madrid!" << endl;
+    cout<<endl;
+
+    cout << "Hala Madrid!" << endl;
 
     return 0;
 }
